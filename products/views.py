fd2175044d223c442953818e75d04f0f14dd678e
noemichis <noemi_chis@yahoo.com>
2023-08-24 08:04:52 +0000

    Add edit product functionality

--- products/views.py
+++ products/views.py
@@ -8,70 +8,70 @@ from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
-  """ 
-  Shows all products, includes sorting and listing
-  """
-  products = Product.objects.all()
-  query = None
-  categories = None
-  sort = None
-  direction = None
-  
-  
-  
-  if request.GET:
-    if 'sort' in request.GET:
-        sortkey = request.GET['sort']
-        sort = sortkey
-        if sortkey == 'name':
-            sortkey = 'lower_name'
-            products = products.annotate(lower_name=Lower('name'))
-        if sortkey == 'category':
-            sortkey = 'category__name'
+    """ 
+    Shows all products, includes sorting and listing
+    """
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+    
+    
+    
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
-        if 'direction' in request.GET:
-            direction = request.GET['direction']
-            if direction == 'desc':
-                sortkey = f'-{sortkey}'
-        products = products.order_by(sortkey)
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
-    if 'category' in request.GET:
-        categories = request.GET['category'].split(',')
-        products = products.filter(category__name__in=categories)
-        categories = Category.objects.filter(name__in=categories)
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-    if 'q' in request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse('products'))
-        
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
-  
-  current_sorting = f'{sort}_{direction}'
-  
-  context = {
-    'products': products,
-    'search_term': query,
-    'current_categories': categories,
-    'current_sorting': current_sorting,
-  }
-  
-  return render(request, 'products/products.html', context)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
+    
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-  """ 
-  Shows individual product details
-  """
-  product = get_object_or_404(Product, pk=product_id)
-  
-  context = {
-    'product': product,
-  }
-  
-  return render(request, 'products/product_detail.html', context)
+    """ 
+    Shows individual product details
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)
 
 
 def add_product(request):
@@ -90,6 +90,30 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
