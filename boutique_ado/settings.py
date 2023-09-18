837e65c8d756af5e1c8accebcb257f8fc5e8b11f
noemichis <noemi_chis@yahoo.com>
2023-09-18 08:05:38 +0000

    Initial heroku deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lk+)2w=6q83+ze^2c*of%wtd$4h^0+=dxcmxqa&=jv4#w)or6k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-noemichis-boutiqueado-ruqwtysjfvt.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['boutique-ado-walkt.herokuapp.com', '8000-noemichis-boutiqueado-ruqwtysjfvt.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
